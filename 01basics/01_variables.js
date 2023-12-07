33669ef0c43dc7ccbc83c190f61cceb04fc0a3c8
Sohan029 <121715317+Sohan029@users.noreply.github.com>
2023-12-07 01:22:05 +0000

    variables

---
+++ 01basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 1234
+let accountEmail = "sohan@com"
+var accountPassword = "sohan1"
+/* 
+   don't prefer var because it has issues with
+   block and functional scopes
+*/
+
+// accountMobile = 12345 it's not a good practice
+
+
+accountEmail = "sc@sc"
+accountPassword ="sac@"
+
+console.table([accountId, accountEmail,accountPassword]);
