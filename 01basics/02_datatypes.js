9d5eccc1101c421e117330e7414ec0c2d331d8f2
Sohan029 <121715317+Sohan029@users.noreply.github.com>
2023-12-07 01:49:38 +0000

    datatypes

---
+++ 01basics/02_datatypes.js
@@ -0,0 +1,18 @@
+let details = null
+// null is a stand alone value.
+console.log(details);
+
+let number
+// undefined is a instance, where variable is declared but value is not intialised.
+console.log(number);
+
+
+console.table([typeof details, typeof number]);
+
+// null is object type and undefined is undefined type
+// ┌─────────┬─────────────┐
+// │ (index) │   Values    │
+// ├─────────┼─────────────┤
+// │    0    │  'object'   │
+// │    1    │ 'undefined' │
+// └─────────┴─────────────┘
