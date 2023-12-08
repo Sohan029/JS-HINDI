c5c95f77707cbfbbaa8b6f047ef42111e70dd86e
Sohan029 <121715317+Sohan029@users.noreply.github.com>
2023-12-08 02:56:47 +0000

    3rd day

---
+++ 01basics/05_datasummary.js
@@ -0,0 +1,39 @@
+// compile time is the phase where the source code is translated into machine code, and it involves checking for syntax errors, type errors, and optimizations. Runtime is the phase during which the compiled program is executed, and it involves the actual processing of the code, including potential errors that might occur during execution. 
+
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof myFunction);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
